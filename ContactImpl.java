abed0ca71dd9903fcd1e063f0d864e14cf019376
Peter Domokos <pdomok01@mail.bbk.ac.uk>
2016-01-21 15:42:11 +0000

    previous 2 commits didn't work. I have now written, tested and passed getNotes and addNotes

--- ContactImpl.java
+++ ContactImpl.java
@@ -1,6 +1,52 @@
 public class ContactImpl implements Contact{
-	int id = 1;
+	int id;
+	String name;
+	String notes;
+	
+	/** 
+	*   A constructor when the user does not have any 
+	*   notes about the contact when they are created.
+	*/
+	public ContactImpl(int id , String name){
+		this.id = id;
+		this.name = name;
+		notes = "";
+	}
+	
+	/** 
+	* A constructor that takes a third argument for notes
+	* that the user wishes to add upon creation of the contact
+	*/
+	public ContactImpl(int id , String name , String notes){
+		this.id = id;
+		this.name = name;
+		this.notes = notes;
+	}
+	
+	/**
+	* @see Contact
+	*/
 	public int getId(){
 		return id;
 	}
+	/**
+	* @see Contact
+	*/
+	
+	public String getName(){
+		return name;
+	}
+	/**
+	* @see Contact
+	*/
+	public String getNotes(){
+			return notes;
+	}
+	/**
+	* @see Contact
+	*/
+	public void addNotes(String note){
+		this.notes += note;
+	}
+	
 }
--- TestContactImpl.java
+++ TestContactImpl.java
@@ -6,13 +6,44 @@ public class TestContactImpl{
 	Contact cont;
 	@Before
 	public void init(){
-		cont = new ContactImpl();
+		cont = new ContactImpl(1, "James Brown");
 	}
 	
 	@Test
 	public void testGetId(){
 		int output = cont.getId();
 		int expected = 1;
+		assertEquals(expected , output);
+	}
+	
+	@Test
+	public void testGetName(){
+		String output = cont.getName();
+		String expected = "James Brown";
+		assertEquals(expected , output);
+	}
+	
+	@Test 
+	public void testGetNotesWhenNull(){
+		String output = cont.getNotes();
+		String expected = "";
+		assertEquals(expected, output);
+	}
+	
+	@Test
+	public void testsAddNotesWhenNotesNullAndGetNotes(){
+		cont.addNotes("Meeting went well.");
+		String output = cont.getNotes();
+		String expected = "Meeting went well.";
+		assertEquals(expected , output);
+	}
+	
+	@Test
+	public void testsAddNotesWhenNotesNotNullAndGetNotes(){
+		cont.addNotes("Meeting went well");
+		cont.addNotes("- actions all agreed");
+		String output = cont.getNotes();
+		String expected = ("Meeting went well- actions all agreed");
 		assertEquals(expected , output);
 	}
 }
