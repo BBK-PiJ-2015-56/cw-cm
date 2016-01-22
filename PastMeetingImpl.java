febfa675ca247a659ea96031dd6a6f7d1ff47689
Peter Domokos <pdomok01@mail.bbk.ac.uk>
2016-01-22 21:28:34 +0000

    I took away the second constructor in PastMeetingImpl, having seen the update requirements. Also made a note.

--- PastMeetingImpl.java
+++ PastMeetingImpl.java
@@ -21,13 +21,6 @@ public class PastMeetingImpl extends MeetingImpl implements PastMeeting{
 	 
 	 A second constructor for when the user wants to include notes
 	
-	
-	 Note that there appears to me to be a contradiction in the latest requiremnets on moodle,
-	 in that it says there must only one constructoe and that if notes is null then a NPE
-	 must be thrown, but on the other hand the getNotes method allows for teh notes being null.
-	 I have gone with the second option, providing the user with a choice of whether to 
-	 include notes or not with 2 constructors. The original requirements that I had
-	 does not specify that 2 constructors are not allowed.
 	public PastMeetingImpl(int id , Calendar date , Set<Contact> contacts, String notes){
 		super(id , date , contacts);
 		this.notes = notes;
