e528b44b28e1e6d75decc4da110054ba3ba05169
peter domokos <peterdomokos@peters-iMac.home>
2016-01-23 00:58:14 +0000

    test passed

--- ContactManagerImpl.java
+++ ContactManagerImpl.java
@@ -1,3 +1,25 @@
-public class ContactManagerImpl{
+import java.util.Calendar;
+import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
+
+/**
+* @see ContactManager
+*/
+public class ContactManagerImpl implements ContactManager{
+	Set<Contact> contacts;
+	Set<Meeting> meetings;
 	
+	public ContactManagerImpl(){
+		contacts = new HashSet<Contact>();
+		meetings = new HashSet<Meeting>();
+	}
+	public int addNewContact(String name , String notes){
+		contacts.add(new ContactImpl(1, name, notes));
+		return 10;
+	}
+	
+	public Set<Contact> getContacts(){
+		return contacts;
+	}
 }
