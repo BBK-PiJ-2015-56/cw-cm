5749a6a37b8dc6d9f69636e9f35c990abc3ab0c9
Peter Domokos <pdomok01@mail.bbk.ac.uk>
2016-01-24 14:52:41 +0000

    added addFutureMeeting method to interface

--- ContactManager.java
+++ ContactManager.java
@@ -38,5 +38,20 @@ public interface ContactManager {
     * any of the provided IDs does not correspond to a real contact
 	*/
 	Set<Contact> getContacts(int... ids);
+	
+	/**
+	* Add a new meeting to be held in the future.
+	*
+	* An ID is returned when the meeting is put into the system. This
+	* ID must be positive and non-zero.
+	*
+	* @param contacts a list of contacts that will participate in the meeting
+	* @param date the date on which the meeting will take place
+	* @return the ID for the meeting
+	* @throws IllegalArgumentException if the meeting is set for a time
+	* in the past, of if any contact is unknown / non-existent.
+	* @throws NullPointerException if the meeting or the date are null
+	*/	
+	int addFutureMeeting(Set<Contact> contacts, Calendar date);
 }
 
